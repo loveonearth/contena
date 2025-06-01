cffad170495c05fe9ed520f43f2a6fe4263a5efb
HERA <everydaymay21@gmail.com>
2025-06-01 18:30:34 +0900

    monday said but

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("issues/0/images");
+  eleventyConfig.addPassthroughCopy("public/CNAME");
+};
 
   return {
     pathPrefix: "/",
